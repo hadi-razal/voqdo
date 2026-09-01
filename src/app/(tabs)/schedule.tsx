e253b4b6280f6ce3dbbe945e3af40bb2df578fba
hadi-razal <hadhirasal22@gmail.com>
2026-09-01 14:57:27 +0400

    added a search button

--- src/app/(tabs)/schedule.tsx
+++ src/app/(tabs)/schedule.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function Explore() {
+
+  const [search, setSearch] = useState('');
+
   return (
     <View style={styles.container}>
       <View style={styles.search_bar_container}>
         <Text style={styles.heading}>Your Works</Text>
-        <TextInput placeholder="Enter your name" style={styles.input} />
+        <TextInput value={search} onChangeText={setSearch} placeholder="Search for a work" style={styles.input} />
+        <Pressable style={styles.search_button} onPress={() => { setSearch('') }}>
+          <Text style={styles.search_button_text}>Search</Text>
+        </Pressable>
       </View>
 
     </View>
@@ -33,6 +40,21 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  search_button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+    width: '100%',
+    height: 40,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  search_button_text: {
+    color: 'white',
+    fontWeight: '900',
+    fontSize: 16,
+    fontFamily: 'Arial',
+  },
   input: {
     borderWidth: 1,
     borderColor: 'black',
@@ -41,6 +63,6 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 5,
     marginBottom: 10,
-    backgroundColor: 'gray/10',
+    backgroundColor: 'gray/20',
   },
 });
