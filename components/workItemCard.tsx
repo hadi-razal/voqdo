52cf5e1729a74c5b265323fecb9604bdf5b0bb56
hadi-razal <hadhirasal22@gmail.com>
2026-09-04 12:23:38 +0400

    improved desing and work item cards data passing methods

--- components/workItemCard.tsx
+++ components/workItemCard.tsx
@@ -1,13 +1,17 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native'
 
-export default function WorkItemCard() {
+export default function WorkItemCard({ data }: any) {
     return (
 
-        <View style={styles.container}>
-            <Pressable>
-                <Text style={styles.title}>Work Item Card</Text>
-                <Text style={styles.description}>This si the description of the work item card</Text>
-                <Text style={styles.description}>Date: 2026-01-01</Text>
+        <View>
+            <Pressable style={styles.container}>
+                <Text style={styles.title}>{data.title}</Text>
+                <Text style={styles.description}>{data.description}</Text>
+                <View style={styles.date_container}>
+                    <Text style={styles.date}>{data.time}</Text>
+                    <Text style={styles.date}>{data.date}</Text>
+                </View>
+
             </Pressable>
         </View>
     )
@@ -17,21 +21,38 @@ const styles = StyleSheet.create({
     container: {
         backgroundColor: 'gray/50',
         width: '100%',
-        borderRadius: 10,
+        borderRadius: 5,
+        borderWidth: 0.3,
+        borderColor: 'gray',
+        paddingHorizontal: 10,
         marginVertical: 3,
-        padding: 10,
+        paddingVertical: 10,
         justifyContent: 'center',
-        alignItems: 'center',
-        borderWidth: 1,
-        borderColor: 'black',
+        alignItems: 'flex-start',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.25,
+        shadowRadius: 3.84,
+        elevation: 5,
     },
     title: {
-        fontSize: 20,
-        fontWeight: 'bold',
+        fontSize: 18,
+        fontWeight: 'semibold',
         color: 'black',
     },
     description: {
         fontSize: 16,
-        color: 'black',
+        color: 'black/80',
+        fontWeight: 200,
+        marginBottom: 10,
+    }, date_container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '100%',
+    },
+    date: {
+        fontSize: 12,
+        color: 'gray',
     },
 })
